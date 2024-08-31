38f61af9c718ad644227b3b42f0bdcf55c8ea316
Andrey Talanin <andrey.talanin@outlook.com>
2024-08-31 16:38:28 +0300

    Add the Word, WordGroup, WordTag database-level entities

---
+++ Source/JapaneseLanguageTools.Data/Entities/WordGroup.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JapaneseLanguageTools.Data.Entities;
+
+public class WordGroup
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [StringLength(256)]
+    public string Caption { get; set; } = string.Empty;
+
+    [StringLength(2048)]
+    public string? Comment { get; set; }
+
+    public bool Enabled { get; set; }
+
+    /// <remarks>
+    /// Do not decorate with the <see cref="DatabaseGeneratedAttribute" /> attribute using the <see cref="DatabaseGeneratedOption.Computed" /> option
+    /// as it allows Entity Framework Core to skip the column on updates, and SQLite does not provide an option to update columns automatically.
+    /// </remarks>
+    public DateTimeOffset CreatedOn { get; set; }
+
+    /// <remarks>
+    /// Do not decorate with the <see cref="DatabaseGeneratedAttribute" /> attribute using the <see cref="DatabaseGeneratedOption.Computed" /> option
+    /// as it allows Entity Framework Core to skip the column on updates, and SQLite does not provide an option to update columns automatically.
+    /// </remarks>
+    public DateTimeOffset UpdatedOn { get; set; }
+
+    public IList<Word> Words { get; set; } = new List<Word>();
+}
